df59cf5c81a79246ad9daa44b2841cddd8d93355
钟梓灏 <1017642638@qq.com>
2017-07-08 16:13:46 +0800

    update

--- Classes/FightMode.h
+++ Classes/FightMode.h
@@ -13,13 +13,6 @@ public:
 	void initAnimation();
 	CREATE_FUNC(FightMode);
 
-	void preloadMusic();
-	void playBgm();
-	void pause();
-	void returnGameCallback(Ref* pSender);
-	void reStartCallback(Ref* pSender);
-	void returnMenuCallback(Ref* pSender);
-
 	void player1AttackByHand(Ref* pSender);
 	void FightMode::player1AttackByLeg(Ref* pSender);
 	void FightMode::player1MoveAnimation();
@@ -38,12 +31,12 @@ public:
 
 
 private:
-	//画面更新
+	//鐢婚潰鏇存柊
 	Size visibleSize;
 	void update(float f);
 	int x_num;
 	int y_num;
-	//player1动画
+	//player1鍔ㄧ敾
 	Sprite * player1;
 	Sprite * qigong1;
 	Vector<SpriteFrame*> player1Idle;
@@ -53,11 +46,11 @@ private:
 	Vector<SpriteFrame*> player1Move;
 	Vector<SpriteFrame*> player1Dead;
 	Vector<SpriteFrame*> player1BeingAttacked;
-	Vector<SpriteFrame*> player1Qigong;//气功形状变化动画
+	Vector<SpriteFrame*> player1Qigong;//姘斿姛褰㈢姸鍙樺寲鍔ㄧ敾
 	Vector<SpriteFrame*> player1Defense;
 	bool player1IsDefend;
 
-	//palyer2动画
+	//palyer2鍔ㄧ敾
 	Sprite * player2;
 	Sprite * qigong2;
 	Vector<SpriteFrame*> player2Idle;
@@ -67,11 +60,11 @@ private:
 	Vector<SpriteFrame*> player2Move;
 	Vector<SpriteFrame*> player2Dead;
 	Vector<SpriteFrame*> player2BeingAttacked;
-	Vector<SpriteFrame*> player2Qigong;//气功形状变化动画
+	Vector<SpriteFrame*> player2Qigong;//姘斿姛褰㈢姸鍙樺寲鍔ㄧ敾
 	Vector<SpriteFrame*> player2Defense;
 	bool player2IsDefend;
 
-	//人物移动相关变量
+	//浜虹墿绉诲姩鐩稿叧鍙橀噺
 	char player1ADMovekey;
 	char player1WSMovekey;
 	char lastkey1;
@@ -91,13 +84,13 @@ private:
 	void onKeyReleased2(EventKeyboard::KeyCode code, Event *event);
 
 
-	//控制人物动作特定函数
+	//鎺у埗浜虹墿鍔ㄤ綔鐗瑰畾鍑芥暟
 	void update_numHit(float f);
 	void update_maxHit(float f);
 	void update_powerHit(float f);
 	int attack(Sprite*, Sprite*, int, bool, bool, bool, bool, ProgressTimer*, ProgressTimer*);
 	int power_attack(Sprite*, Sprite*, Sprite*, Sprite*, bool, bool, ProgressTimer*, ProgressTimer*);
-	//人物操作相关变量
+	//浜虹墿鎿嶄綔鐩稿叧鍙橀噺
 	int player1_maxHit = 0;
 	int player1_numHit = 0;
 	bool player1_attack_1 = false;
